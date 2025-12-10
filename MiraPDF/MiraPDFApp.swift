8f0951aed763028b043dce38c3262d560341ea32
suiyun39 <suiyun39@gmail.com>
2025-12-10 11:12:02 +0800

    chore: 初始化工程

---
+++ MiraPDF/MiraPDFApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiraPDFApp.swift
+//  MiraPDF
+//
+//  Created by 随云 on 2025/12/10.
+//
+
+import SwiftUI
+
+@main
+struct MiraPDFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
