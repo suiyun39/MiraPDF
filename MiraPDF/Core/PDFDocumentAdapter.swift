28263baa5dc0c080091fb8c1b4856f33560fb137
suiyun39 <suiyun39@gmail.com>
2026-03-17 17:46:46 +0800

    feat: 重整文档适配器，完善错误处理信息

--- MiraPDF/Core/PDFDocumentAdapter.swift
+++ MiraPDF/Core/PDFDocumentAdapter.swift
@@ -2,6 +2,28 @@ import OSLog
 import SwiftUI
 import UniformTypeIdentifiers
 
+enum PDFDocumentAdapterError: LocalizedError {
+  case unknown
+  case emptyFile
+  case corruptFile
+
+  var failureReason: String? {
+    switch self {
+    case .unknown: String(localized: "error.unknown.reason", table: "ErrorMessage")
+    case .emptyFile: String(localized: "error.empty_file.reason", table: "ErrorMessage")
+    case .corruptFile: String(localized: "error.corrupt_file.reason", table: "ErrorMessage")
+    }
+  }
+
+  var recoverySuggestion: String? {
+    switch self {
+    case .unknown: nil
+    case .emptyFile: nil
+    case .corruptFile: String(localized: "error.corrupt_file.suggestion", table: "ErrorMessage")
+    }
+  }
+}
+
 /// DocumentGroup 文档适配器，应仅用于处理 pdf 文件读取与写入
 struct PDFDocumentAdapter: FileDocument {
   static var readableContentTypes: [UTType] {
@@ -10,35 +32,30 @@ struct PDFDocumentAdapter: FileDocument {
 
   private let logger = Logger(category: "Core")
 
-  /// pdf 文件数据
-  var data = Data()
-
   // 仅用于满足 DocumentGroup 类型签名，此处不应有任何逻辑
   init() {
   }
 
-  // todo: 是否应在此处读取数据与验证？
   init(configuration: ReadConfiguration) throws {
     logger.info("打开文件「\(configuration.file.filename ?? "未命名")」")
 
+    // 空数据检查
     guard let data = configuration.file.regularFileContents else {
-      logger.error("读取文件数据失败")
-      throw CocoaError(.fileReadCorruptFile)
+      logger.error("文件数据读取失败")
+      throw PDFDocumentAdapterError.unknown
     }
-
     guard !data.isEmpty else {
       logger.error("文件数据为空")
-      throw CocoaError(.fileReadCorruptFile)
+      throw PDFDocumentAdapterError.emptyFile
     }
 
-    // 检查 pdf 文件头，[0x25, 0x50, 0x44, 0x46] -> "%PDF"
-    guard data.starts(with: [0x25, 0x50, 0x44, 0x46]) else {
-      logger.error("不是有效的 pdf 文件")
-      throw CocoaError(.fileReadCorruptFile)
+    // 文件头检查，[0x25, 0x50, 0x44, 0x46, 0x2d] -> "%PDF-"
+    guard data.starts(with: [0x25, 0x50, 0x44, 0x46, 0x2d]) else {
+      logger.error("文件头校验错误，不是有效的 pdf 文件")
+      throw PDFDocumentAdapterError.corruptFile
     }
 
     logger.info("文件加载成功，文件大小 \(data.count) 字节")
-    self.data = data
   }
 
   func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
